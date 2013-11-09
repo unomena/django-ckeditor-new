04454d324958f347f2e3a686c4570e6a1c94c104
Michael Whelehan <michael@unomena.com>
2013-11-09 21:25:34 +0200

    changed to json

--- ckeditor/widgets.py
+++ ckeditor/widgets.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
 from django.utils.encoding import force_unicode
-from django.utils import simplejson
+from django.utils import json
 
 from django.core.exceptions import ImproperlyConfigured
 from django.forms.util import flatatt
